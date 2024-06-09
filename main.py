8206e278c1af0c98a17ede4d4f213d3ab4430986
GerritFischer <gerrit.fischer2@icloud.com>
2024-06-09 18:50:52 +0200

    added individual spike counts and visualization via opacity of regions

--- main.py
+++ main.py
@@ -12,39 +12,43 @@ from brainbox.plot import peri_event_time_histogram
 from brainbox.singlecell import calculate_peths
 from one.api import ONE
 from ibllib.atlas import AllenAtlas
+import math
 one = ONE(base_url='https://openalyx.internationalbrainlab.org', password='international', silent=True)
 ba = AllenAtlas()
+
 
 def get_n_random_points_in_region(region, N):
     """
     Gets N random points inside (or on the surface) of a mesh
     """
-
     region_bounds = region.mesh.bounds()
     X = np.random.randint(region_bounds[0], region_bounds[1], size=10000)
     Y = np.random.randint(region_bounds[2], region_bounds[3], size=10000)
     Z = np.random.randint(region_bounds[4], region_bounds[5], size=10000)
+    print("Region Bound X:" + str(region_bounds[0]) + " " + str(region_bounds[1]))
+    X = [math.floor(clusters.x[2] * 2500000), math.floor(clusters.x[3] * 2500000)]
+    Y = [math.floor(clusters.y[2] * 2500000), math.floor(clusters.y[3] * 2500000)]
+    Z = [math.floor(clusters.z[2] * 2500000), math.floor(clusters.z[3] * 2500000)]
+    print(str(X) + " " + str(Y) + " " + str(Z))
     pts = [[x, y, z] for x, y, z in zip(X, Y, Z)]
 
     ipts = region.mesh.inside_points(pts).coordinates
-    return np.vstack(random.choices(ipts, k=N))
-# Display the Allen Brain mouse atlas.
-scene = Scene(atlas_name="allen_mouse_25um", title="Cells in primary visual cortex")
-
-# Display a brain region
-primary_visual = scene.add_brain_region("SI", alpha=0.2)
-
-# Get a numpy array with (fake) coordinates of some labelled cells
-coordinates = get_n_random_points_in_region(primary_visual, 2000)
-
-# Create a Points actor
+    print(np.vstack((X,Y,Z)))
+    return np.vstack((X,Y,Z))
 
 
-cells = Points(coordinates)
 
-# Add to scene
-scene.add(cells)
-
+def loadData():
+    roi = "SI" # Region Of Interest (acronym according to Allen Atlas)
+    ses = one.alyx.rest('insertions', 'list', atlas_acronym = roi) #loading recordings
+    print(f'Found {len(ses)} recordings')
+    #isolating first session and probe ID
+    EID = ses[0]['session']
+    PID = ses[0]['id']
+    sl = SpikeSortingLoader(pid=PID, one=one, atlas=ba) # create instance of the SpikeSortingLoader
+    spikes, clusters, channels = sl.load_spike_sorting() # load in spike sorted data
+    clusters = sl.merge_clusters(spikes, clusters, channels) # merge cluster metrics
+    return spikes, clusters, channels
 
 
 
@@ -55,17 +59,15 @@ def button_play_pause(obj, btn):
         # instruct to call handle_timer() every 10 msec:
         timer_id = plotter.timer_callback("create", dt=1000)
     button.switch() 
-roi = "SI" # Region Of Interest (acronym according to Allen Atlas)
-ses = one.alyx.rest('insertions', 'list', atlas_acronym = roi) #loading recordings
-print(f'Found {len(ses)} recordings')
-#isolating first session and probe ID
-EID = ses[0]['session']
-PID = ses[0]['id']
-sl = SpikeSortingLoader(pid=PID, one=one, atlas=ba) # create instance of the SpikeSortingLoader
-spikes, clusters, channels = sl.load_spike_sorting() # load in spike sorted data
-clusters = sl.merge_clusters(spikes, clusters, channels) # merge cluster metrics
 
 
+
+scene = Scene(atlas_name="allen_mouse_25um", title="OND-Visualization")
+spikes, clusters, channels = loadData()
+
+regionModels = []
+for acro in list(set(clusters.acronym)):
+    regionModels.append([acro, scene.add_brain_region(acro, alpha=0.5)])
 
 
 start = 0
@@ -74,6 +76,7 @@ timer = 0
 i = 0
 
 def animation_tick(event): 
+    global regionModels
     global timer
     global end
     global i
@@ -89,41 +92,41 @@ def animation_tick(event):
             else:
                 elemStillIn = False
             i += 1
-    
-        text.text(str(len(currentSpikes)))
-        print("hel")
-        print(clusters.z[0])
+
+        outputString = "Total Spikes: " + str(len(currentSpikes)) + "\n"
+        #print(spikes.clusters.keys())
+        for k in range(len(regionModels)):
+            spikesInRegion = 0
+            for j in currentSpikes:
+                if(clusters.acronym[spikes.clusters[j]] == regionModels[k][0]):
+                    spikesInRegion += 1
+            outputString += "Spikes in " + regionModels[k][0] + ": " + str(spikesInRegion) + "\n"
+            plotter.get_actors()[k+2].GetProperty().SetOpacity(spikesInRegion * 0.001)
+            print(spikesInRegion)
+            #regionModels[i][1].SetAlpha(spikesInRegion * 0.01)
+        outputString += "Time: " + str(timer)
+        text.text(outputString)
         timer += 1 
     global time_e  
-    boop = plotter.get_actors()[2]
-    boop.SetVisibility(not boop.GetVisibility())
+    print(plotter.get_actors())
+    #boop = plotter.get_actors()[2]
+    #boop.SetVisibility(not boop.GetVisibility())
     time_e += 1
     plotter.render()
 
 
 
 
-   
-
-
-
-
-
-#scene.add_label(scene.get_actors()[0], "Testing")
-
 time_e = 0
 timer_id = -1
 t0 = time.time()
 
 plotter= Plotter()
-# plt.initialize_interactor() # on windows this is needed
+
 
 button = plotter.add_button(button_play_pause, states=[" Play ","Pause"], size=40)
 evntid = plotter.add_callback("timer", animation_tick, enable_picking=False)
 text = Text2D("Test")
 plotter.add(text)
-elp = Ellipsoid((20,20,20))
-#plotter.add(elp)
-#lbox = plotter.add_legend_box()
-
-plotter.show(elp, __doc__, scene.get_actors())
+plotter.roll(180)
+plotter.show( __doc__, scene.get_actors())
